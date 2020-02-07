73de8ce4894c4cade1ed5e03894d73b63d747d5e
anhyeuem1801 <son73462385@gmail.com>
2020-02-07 18:14:59 +0700

    first commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speedEnemy;
+    private Rigidbody enemyRb;
+    private GameObject playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerRb = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (playerRb.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection* speedEnemy);
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
